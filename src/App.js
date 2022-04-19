c3022f075a1f17c970a0ec19da56131c298cc8da
Biljeg <biljeg8@outlook.com>
2022-04-19 16:25:23 +0200

    minor tweaks and gh pages setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import {useState} from "react"
+import "./App.css"
+import { useState } from "react"
 import Overlay from "./components/overlay/Overlay.component"
 import Quiz from "./components/quiz/Quiz.component"
 
 function App() {
-  const [overlay, setOverlay] = useState(false)
-  function startGame(){
-    setOverlay(false)
-  }
-  return (
-    overlay ? <Overlay handleClick={startGame}/> : <Quiz startNewGame={setOverlay}/>
-  );
+	const [overlay, setOverlay] = useState(true)
+	function startGame() {
+		setOverlay(false)
+	}
+	return overlay ? (
+		<Overlay handleClick={startGame} />
+	) : (
+		<Quiz startNewGame={setOverlay} />
+	)
 }
 
-export default App;
+export default App
--- src/components/overlay/Overlay.component.jsx
+++ src/components/overlay/Overlay.component.jsx
@@ -3,14 +3,21 @@ import blobYellow from "../../assets/blob-yellow.png"
 import "./Overlay.styles.scss"
 import Button from "../button/Button.component"
 
-export default function Overlay({handleClick}){
-    return(
-        <div className="overlay">
-            <img className="overlay--yellow" alt="" src={blobYellow} aria-hidden="true"/>
-            <img className="overlay--blue" alt="" src={blobBlue} aria-hidden="true"/>
-            <h1 className="overlay--heading">Quizzical</h1>
-            <p className="overlay--description">A general trivia quiz</p>
-            <Button fontSize="16px" fontWeight={"400"} handleClick={handleClick}>Start quiz</Button>
-        </div>
-    )
+export default function Overlay({ handleClick }) {
+	return (
+		<div className="overlay">
+			<img
+				className="overlay--yellow"
+				alt=""
+				src={blobYellow}
+				aria-hidden="true"
+			/>
+			<img className="overlay--blue" alt="" src={blobBlue} aria-hidden="true" />
+			<h1 className="overlay--heading">Quizzical</h1>
+			<p className="overlay--description">A general trivia quiz</p>
+			<Button fontSize="16px" fontWeight="400" handleClick={handleClick}>
+				Start quiz
+			</Button>
+		</div>
+	)
 }
